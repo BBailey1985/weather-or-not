b1253056a98a232c349d435ba3671468bfa60256
BBailey1985 <brandon_bailey@hotmail.com>
2021-12-13 23:38:59 -0600

    started 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
+//variables
 var userFormEl = document.querySelector("#user-form");
 var searchButtonEl = document.querySelector("#search-button");
 var searchCityNameEl = document.querySelector("#search-city-name");
-var stationinfoEl = document.querySelector("#station-info");
+var currentWeatherEl = document.querySelector("#current-weather");
 var cityNameEl = document.querySelector("#city-name");
 var weatherPicEl = document.querySelector("#weather-pic");
 var tempEl = document.querySelector("#temp");
 var windEl = document.querySelector("#wind");
 var humidityEl = document.querySelector("#humidity");
 var uvIndex = document.querySelector("#uv-index");
+var fiveDayEl = document.querySelector("#five-day");
 
 // API key to pull in data
 var apiKey = "1c06fdea9753395ddbde291bde578a40";
@@ -20,6 +22,7 @@ var getWeather = function(cityName) {
   //fetch the API and then display data
   fetch(apiUrl).then(function(response) {
     response.json().then(function(data) {
+      currentWeatherEl.classList.remove("d-none");
       console.log(data);
       //current weather 
       var currentDay = moment().format("L"); 
@@ -55,8 +58,40 @@ var getWeather = function(cityName) {
         uvIndex.append(uvIndexSpan);
         }); 
       });
+
+      // 5 day forecast section
+      var fiveDayApiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + apiKey;
+      // fetch new API for 5 day forecast
+      fetch(fiveDayApiUrl).then(function(response) {
+        response.json().then(function(data) {
+          console.log(data);
+          fiveDayEl.classList.remove("d-none");
+          var outlookEl = document.querySelectorAll(".outlook");
+
+          //loop to add forecasts
+          for (i = 0; i < outlookEl.length; i++) {
+            outlookEl.textContent = "";
+            var plusOneDay = moment().add(i, "days").format("MM-DD-YYYY");
+            var outlookSpan = document.createElement("p");
+            outlookSpan.setAttribute("class", "mt-3 mb-0");
+            outlookSpan.textContent = plusOneDay;
+            outlookEl[i].append(outlookSpan);
+          }
+
+
+
+        });
+
+      });
+
+
+
+
+
+
     });
   });
+
 }
 
 
